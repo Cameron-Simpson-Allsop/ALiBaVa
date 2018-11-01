189950b8464634cdd6414b36d9a42c8d5de4f8a5
csimpson <c.simpson@cern.ch>
2018-11-01 15:56:59 +0000

    update

--- ATLAS17minis_GammaIrrad/I-V/ATLAS17_IV.c
+++ ATLAS17minis_GammaIrrad/I-V/ATLAS17_IV.c
@@ -11,7 +11,7 @@ void TempScale(TString file, double temp, TString rad){
   ifstream infile;
   ofstream outfile;
   infile.open(file);
-  outfile.open("Pre_Anneal/ATLAS17_"+rad+"_IV_scaled.txt");
+  outfile.open("ATLAS17_"+rad+"_IV_scaled.txt");
   while(!infile.eof())
     {
       double reftemp = 248.; //-25C
@@ -39,6 +39,7 @@ void ATLAS17_IV(){
   TString Pre_10Mrad = "Pre_Anneal/ATLAS17_10Mrad_PreAnneal_IV_25-10-2018_Processed.txt";
   TString Pre_17p5Mrad = "Pre_Anneal/ATLAS17_17p5Mrad_PreAnneal_IV_30-10-18_Processed.txt";
   TString Pre_35Mrad = "Pre_Anneal/ATLAS17_35Mrad_PreAnneal_IV_31-10-2018_Processed.txt";
+  TString Post_35Mrad = "Post_Anneal/ATLAS17_35Mrad_PostAnneal_IV_31-10-18_Processed.txt";
   
   TString input = "";
   std::cout << "Scale Temperature? (y/n)" << std::endl;
@@ -54,22 +55,30 @@ void ATLAS17_IV(){
   if(input == "y"){
     std::cout <<"\n10Mrad_PreAnneal (Unscaled vs Scaled)\n\n"; TempScale(Pre_10Mrad,-26.7,"10Mrad_PreAnneal");
     std::cout <<"\n17.5Mrad_PreAnneal (Unscaled vs Scaled)\n\n"; TempScale(Pre_17p5Mrad,-26.4,"17p5Mrad_PreAnneal");
-    std::cout <<"\n35Mrad_PreAnneal (Unscaled vs Scaled)\n\n"; TempScale(Pre_35Mrad,-26.3,"35Mrad_PreAnneal"); 
-    Pre_10Mrad = "Pre_Anneal/ATLAS17_10Mrad_PreAnneal_IV_scaled.txt";
-    Pre_17p5Mrad = "Pre_Anneal/ATLAS17_17p5Mrad_PreAnneal_IV_scaled.txt";
-    Pre_35Mrad = "Pre_Anneal/ATLAS17_35Mrad_PreAnneal_IV_scaled.txt";
+    std::cout <<"\n35Mrad_PreAnneal (Unscaled vs Scaled)\n\n"; TempScale(Pre_35Mrad,-26.3,"35Mrad_PreAnneal");
+    std::cout <<"\n35Mrad_PostAnneal (Unscaled vs Scaled)\n\n"; TempScale(Post_35Mrad,-24.1,"35Mrad_PostAnneal"); 
+    Pre_10Mrad = "ATLAS17_10Mrad_PreAnneal_IV_scaled.txt";
+    Pre_17p5Mrad = "ATLAS17_17p5Mrad_PreAnneal_IV_scaled.txt";
+    Pre_35Mrad = "ATLAS17_35Mrad_PreAnneal_IV_scaled.txt";
+    Post_35Mrad = "ATLAS17_35Mrad_PostAnneal_IV_scaled.txt";
     
     TGraphErrors *Pre10Mrad = new TGraphErrors(Pre_10Mrad);
     Pre10Mrad->SetMarkerColor(kBlack);
     Pre10Mrad->SetLineColor(kBlack);
-    
+    Pre10Mrad->SetMarkerStyle(22);
     TGraphErrors *Pre17p5Mrad = new TGraphErrors(Pre_17p5Mrad);
     Pre17p5Mrad->SetMarkerColor(kBlue);
     Pre17p5Mrad->SetLineColor(kBlue);
-    
+    Pre17p5Mrad->SetMarkerStyle(22);
     TGraphErrors *Pre35Mrad = new TGraphErrors(Pre_35Mrad);  
     Pre35Mrad->SetMarkerColor(kRed);
     Pre35Mrad->SetLineColor(kRed);
+    Pre35Mrad->SetMarkerStyle(22);
+ 
+    TGraphErrors *Post35Mrad = new TGraphErrors(Post_35Mrad);  
+    Post35Mrad->SetMarkerColor(kRed);
+    Post35Mrad->SetLineColor(kRed);
+    Post35Mrad->SetMarkerStyle(23);
     
     TMultiGraph *mg = new TMultiGraph();
     //mg->GetXaxis()->SetTitle("Bias Voltage (V)");
@@ -77,8 +86,10 @@ void ATLAS17_IV(){
     mg->Add(Pre10Mrad,"p`");
     mg->Add(Pre17p5Mrad,"p");
     mg->Add(Pre35Mrad,"p");
+    mg->Add(Post35Mrad,"p");
+    
     TLegend *leg;
-    leg = new TLegend(0.2,0.2,0.4,0.3);
+    leg = new TLegend(0.2,0.16,0.4,0.3);
     leg->SetBorderSize(0);
     leg->SetFillColor(0);
     leg->SetTextSize(0.035);
@@ -88,6 +99,7 @@ void ATLAS17_IV(){
     leg->AddEntry(Pre10Mrad,"10Mrad Pre-Annealing","lp");
     leg->AddEntry(Pre17p5Mrad,"17.5Mrad Pre-Annealing","lp");
     leg->AddEntry(Pre35Mrad,"35Mrad Pre-Annealing","lp");
+    leg->AddEntry(Post35Mrad,"35Mrad Post-Annealing","lp");
     TCanvas *IV = new TCanvas("IV_scaled","IV_scaled",600,600);
     mg->Draw("AP");
     leg->Draw("same");
@@ -98,18 +110,25 @@ void ATLAS17_IV(){
     Pre_10Mrad = "Pre_Anneal/ATLAS17_10Mrad_PreAnneal_IV_25-10-2018_Processed.txt";
     Pre_17p5Mrad = "Pre_Anneal/ATLAS17_17p5Mrad_PreAnneal_IV_30-10-18_Processed.txt";
     Pre_35Mrad = "Pre_Anneal/ATLAS17_35Mrad_PreAnneal_IV_31-10-2018_Processed.txt";
+    Post_35Mrad = "Post_Anneal/ATLAS17_35Mrad_PostAnneal_IV_31-10-18_Processed.txt";
     
     TGraphErrors *Pre10Mrad = new TGraphErrors(Pre_10Mrad);
     Pre10Mrad->SetMarkerColor(kBlack);
     Pre10Mrad->SetLineColor(kBlack);
-    
+    Pre10Mrad->SetMarkerStyle(22);
     TGraphErrors *Pre17p5Mrad = new TGraphErrors(Pre_17p5Mrad);
     Pre17p5Mrad->SetMarkerColor(kBlue);
     Pre17p5Mrad->SetLineColor(kBlue);
-    
+    Pre17p5Mrad->SetMarkerStyle(22);
     TGraphErrors *Pre35Mrad = new TGraphErrors(Pre_35Mrad);  
     Pre35Mrad->SetMarkerColor(kRed);
     Pre35Mrad->SetLineColor(kRed);
+    Pre35Mrad->SetMarkerStyle(22);
+ 
+    TGraphErrors *Post35Mrad = new TGraphErrors(Post_35Mrad);  
+    Post35Mrad->SetMarkerColor(kRed);
+    Post35Mrad->SetLineColor(kRed);
+    Post35Mrad->SetMarkerStyle(23);
     
     TMultiGraph *mg = new TMultiGraph();
     //mg->GetXaxis()->SetTitle("Bias Voltage (V)");
@@ -117,8 +136,10 @@ void ATLAS17_IV(){
     mg->Add(Pre10Mrad,"p`");
     mg->Add(Pre17p5Mrad,"p");
     mg->Add(Pre35Mrad,"p");
+    mg->Add(Post35Mrad,"p");
+    
     TLegend *leg;
-    leg = new TLegend(0.2,0.2,0.4,0.3);
+    leg = new TLegend(0.2,0.16,0.4,0.3);
     leg->SetBorderSize(0);
     leg->SetFillColor(0);
     leg->SetTextSize(0.035);
@@ -128,6 +149,7 @@ void ATLAS17_IV(){
     leg->AddEntry(Pre10Mrad,"10Mrad Pre-Annealing -26.7C","lp");
     leg->AddEntry(Pre17p5Mrad,"17.5Mrad Pre-Annealing -26.4C","lp");
     leg->AddEntry(Pre35Mrad,"35Mrad Pre-Annealing -26.3C","lp");
+    leg->AddEntry(Pre35Mrad,"35Mrad Post-Annealing -24.1C","lp");
     TCanvas *IV = new TCanvas("IV_unscaled","IV_unscaled",600,600);
     mg->Draw("AP");
     leg->Draw("same");
--- ATLAS17minis_GammaIrrad/TemperatureCorrection.c
+++ ATLAS17minis_GammaIrrad/TemperatureCorrection.c
@@ -41,15 +41,26 @@ void TemperatureCorrection(){
   Scale("RSRun29Oct18ATLAS17GammaPreAnneal17p5Mrad_1/CCE_output_PF.txt",{-22.6,-23.4,-24.0,-24.4,-24.6,-24.8,-25.0,-25.1,-25.3,-25.7,-25.7},"17p5Mrad_PreAnneal");
   Scale("RSRun30Oct18ATLAS17PreAnneal35Mrad_1/CCE_output_PF.txt",{-23.0,-23.3,-24.1,-24.5,-24.8,-25.0,-25.2,-25.3,-25.4,-25.4,-25.4},"35Mrad_PreAnneal");
 
+  Scale("RSRun31Oct18ATLAS17PostAnneal35Mrad_2/CCE_output_PF.txt",{-21.4,-22.5,-23.2,-23.5,-23.5,-23.7,-23.6,-23.5,-23.8,-24.2,-24.2},"35Mrad_PostAnneal");
+
   TGraphErrors *PreMrad10 = new TGraphErrors("10Mrad_PreAnneal.txt");
   PreMrad10->SetMarkerColor(kBlack);
   PreMrad10->SetLineColor(kBlack);
+  PreMrad10->SetMarkerStyle(22);
   TGraphErrors *PreMrad17p5 = new TGraphErrors("17p5Mrad_PreAnneal.txt");
   PreMrad17p5->SetMarkerColor(kBlue);
   PreMrad17p5->SetLineColor(kBlue);
+  PreMrad17p5->SetMarkerStyle(22);
   TGraphErrors *PreMrad35 = new TGraphErrors("35Mrad_PreAnneal.txt");
   PreMrad35->SetMarkerColor(kRed);
   PreMrad35->SetLineColor(kRed);
+  PreMrad35->SetMarkerStyle(22);
+  
+  TGraphErrors *PostMrad35 = new TGraphErrors("35Mrad_PostAnneal.txt");
+  PostMrad35->SetMarkerColor(kRed);
+  PostMrad35->SetLineColor(kRed);
+  PostMrad35->SetMarkerStyle(23);
+   
   TMultiGraph *Combined_CCE = new TMultiGraph();
   Combined_CCE->GetXaxis()->SetTitle("Bias Voltage [kV]");
   Combined_CCE->GetYaxis()->SetTitle("Collected Charge [ke]");
@@ -57,9 +68,10 @@ void TemperatureCorrection(){
   Combined_CCE->Add(PreMrad10,"p");
   Combined_CCE->Add(PreMrad17p5,"p");
   Combined_CCE->Add(PreMrad35,"p");
+  Combined_CCE->Add(PostMrad35,"p");
   
   TLegend *leg;
-  leg = new TLegend(0.5,0.2,0.7,0.3);
+  leg = new TLegend(0.5,0.16,0.7,0.3);
   leg->SetBorderSize(0);
   leg->SetFillColor(0);
   leg->SetTextSize(0.035);
@@ -69,6 +81,7 @@ void TemperatureCorrection(){
   leg->AddEntry(PreMrad10,"Pre-Annealing 10Mrad","lp");
   leg->AddEntry(PreMrad17p5,"Pre-Annealing 17.5Mrad","lp");
   leg->AddEntry(PreMrad35,"Pre-Annealing 35Mrad","lp");
+  leg->AddEntry(PostMrad35,"Post-Annealing 35Mrad","lp");
   
   TGaxis::SetMaxDigits(2);
   
